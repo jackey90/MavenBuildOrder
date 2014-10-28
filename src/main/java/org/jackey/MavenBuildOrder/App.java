56b56df9b95d3c3ce4de2c6c9bcfb1d1100348b9
Jackey <jackey90.hj@gmail.com>
2014-10-29 00:08:31 +0800

    firstcommit

---
+++ src/main/java/org/jackey/MavenBuildOrder/App.java
@@ -0,0 +1,13 @@
+package org.jackey.MavenBuildOrder;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
